98d6cc8dfca1033199af313907cd2834410324a3
aloulou1238 <benhibaali@gmail.com>
2022-02-05 11:35:14 +0100

    About page updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
 function About() {
   return <div>
     <h1 className="text-6xl mb-4">Github Finder</h1>
+    <p>This is a Github finder where you can search Github profiles and get their information</p>
+    <p className="pt-4">Created by Ali Ben Hiba</p>
   </div>;
 }
 
